de40ba80d463b3aae48ea1cff28e24bfc9837103
aumat <aumat@AUMAR>
2024-06-26 21:36:42 +0800

    Add project files.

---
+++ WeatherForecast.cs
@@ -0,0 +1,14 @@
+namespace PREFINALS_AUTH_Espinas_Annielyn
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
+
